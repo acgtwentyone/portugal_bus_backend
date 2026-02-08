e9b144bc76504e77b72e1be84a59f019d09dc12b
acgtwentyone <acgtwentyone@gmail.com>
2026-02-08 12:17:55 +0000

    Add route for welcome view

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BusLineController;
 
+Route::get('/', function() {
+    return view('welcome');
+});
+
 Route::prefix('admin')->group(function() {
     Route::get('/lines', [BusLineController::class, 'index'])->name('admin.lines.index');
     Route::post('/lines/forceSync', [BusLineController::class, 'forceSync'])->name('admin.busLines.forceSync');
