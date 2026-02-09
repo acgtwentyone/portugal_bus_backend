03ec0863c716ccd39d0fa62058445bb3887d49d1
acgtwentyone <acgtwentyone@gmail.com>
2026-02-09 20:17:00 +0000

    fix: issues fixed

--- app/Http/Resources/BusLineResource.php
+++ app/Http/Resources/BusLineResource.php
@@ -14,6 +14,11 @@ class BusLineResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'code' => $this->code,
+            'name' => $this->name,
+            'network' => $this->network,
+        ];
     }
 }
